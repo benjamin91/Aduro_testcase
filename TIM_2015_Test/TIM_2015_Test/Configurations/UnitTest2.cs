3d82116291e98f8987ba9debee8240c2c4dc1d30
locnguyen <thienloc.nguyen91@gmail.com>
2018-08-13 21:49:55 +0700

    add UnitTest 2

---
+++ TIM_2015_Test/TIM_2015_Test/Configurations/UnitTest2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TIM_2015_Test.Configurations
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Console.WriteLine("Test Method 3");
+        }
+    }
+}
